c0aa9510fbe6c980a11db29eae6b9606edef5b59
Fabricio Cervantes <fabriciocervantesmendoza@gmail.com>
2026-01-03 17:23:06 -0500

    feat: Inicializar proyecto para front y back

---
+++ games-ti/src/test/java/com/games/games_ti/GamesTiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.games.games_ti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesTiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
