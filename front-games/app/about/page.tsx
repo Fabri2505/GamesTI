902a894da8b8cd65b48f68bdcdda36e0b88d155d
Fabricio Cervantes <fabriciocervantesmendoza@gmail.com>
2026-01-03 23:26:17 -0500

    feat: Organizacion de carpetas

--- front-games/app/about/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function About() {
-    return (
-      <div className="p-8">
-        <h1 className="text-3xl font-bold">Sobre Nosotros</h1>
-        <p className="mt-4">Esta es la página About</p>
-      </div>
-    );
-}
