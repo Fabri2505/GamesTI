051f1521301a21892dfef2021bc237f6195f5e6c
Fabricio Cervantes <fabriciocervantesmendoza@gmail.com>
2026-01-05 23:05:15 -0500

    feat: Mover componente para el menu

--- front-games/app/menu/page.tsx
+++ front-games/app/menu/page.tsx
@@ -1,3 +1,36 @@
+"use client";
+
+import { CardSelectGame } from "@/components/card-game-select";
+import Header from "@/components/header";
+import { useRouter } from "next/navigation";
+
 export default function MenuPage() {
-    return <div>Menu Page</div>;
+  const router = useRouter();
+    return (
+      <div>
+        <Header titulo="Games-TI"></Header>
+        <section className="pt-12 pb-12 px-6 mx-auto text-center">
+          <h1 className="text-6xl md:text-8xl font-black tracking-tighter text-balance">
+            ¿Quién será el <span className="text-blue-500 italic">barbón de la semana?</span>
+          </h1>
+          <p className="text-muted-foreground text-lg max-w-xl mx-auto text-pretty">
+            Elige juego favorito. Jugadas de alta estrategia o la emoción pura del Bingo.
+          </p>
+        </section>
+        <section className="flex gap-4 justify-center">
+          <CardSelectGame 
+            title="Golpeado"
+            subtitle="ESTRATEGIA"
+            imageUrl="/Golpeado-Logo.png"
+            onPlay={() => router.push('/golpeado')}
+          />
+          <CardSelectGame 
+            title="Bingo"
+            subtitle="Azar y Suerte"
+            imageUrl="/Bingo-Logo.png"
+            onPlay={() => router.push('/bingo')}
+          />
+        </section>
+      </div>
+    );
 }
--- front-games/components/header.tsx
+++ front-games/components/header.tsx
@@ -7,6 +7,14 @@ export default function Header(promps : {titulo:string}){
   return (
     <header className=" py-2 px-9 border-b border-gray-700 flex justify-between items-center">
       <h1 className="text-2xl font-bold">{promps.titulo}</h1>
+      <nav className="flex gap-4 justify-center">
+        <a href="/menu" className="text-gray-300 hover:text-white">
+          Dashboard pagos
+        </a>
+        <a href="/about" className="text-gray-300 hover:text-white">
+          Historial
+        </a>
+      </nav>
       <div className="flex items-center gap-4">
         <Button variant="ghost" size="icon" className="rounded-full">
           <Trophy className="w-10 h-10" />
